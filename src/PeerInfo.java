2760bea0355abfa907e149feb2a5eef11b6a852e
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-02-26 00:09:51 -0500

    Update PeerInfo.java

--- src/PeerInfo.java
+++ src/PeerInfo.java
@@ -8,7 +8,7 @@ public class PeerInfo {
 
 	protected static int unchokeInterval;
 	protected static int optimisticUnchokeInterval;	
-	public static ArrayList <Integer>	chokedInterested;
+	public static ArrayList <Integer>  chokedInterested;  //List of neighbours who would be interested. (Used in OptChoke class)
 	public static int optimisticUnchokedPeer;
 	public static LoggerPeer log;
 	public static OptUnchokeTimer optUnchokeTimer;
