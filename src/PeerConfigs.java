479dde0148a484e44a9571816fe38b5be3083b1f
Gloria <gmwBits@gmail.com>
2015-04-05 20:21:52 -0400

    Updated file processing functions.

--- src/PeerConfigs.java
+++ src/PeerConfigs.java
@@ -7,6 +7,7 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.*;
 
 public class PeerConfigs {
@@ -88,73 +89,62 @@ public class PeerConfigs {
 	public void readCommonConfig(String commonConfigPath) throws Exception
 	{
 		try{
-			int token;
+			in = new FileInputStream(commonConfigPath);
+			Scanner scanner = new Scanner(in);
 			
-			in = new FileInputStream(commonConfigPath);	
-			streamReader = new BufferedReader(new InputStreamReader(in));
-			tokenizer = new StreamTokenizer(streamReader);
-			tokenizer.eolIsSignificant(true);
+			String token = "";
 			
-			boolean eof = false;
+			while(scanner.hasNext())
+			{
+				token = scanner.next();
+				if(token.compareToIgnoreCase("NumberOfPreferredNeighbors") == 0)
+				{
+					if(!scanner.hasNextInt())
+						throw new Exception("Error:  Integer value required for the number of preferred neighbors.");
+					else
+						prefNeighbors = scanner.nextInt();
+				}
+				else if(token.compareToIgnoreCase("UnchokingInterval") == 0)
+				{
+					if(!scanner.hasNextInt())
+						throw new Exception("Error:  Integer value required for the preferred neighbor selection interval.");
+					else
+						timeUnchoke = scanner.nextInt();
+				}
+				else if(token.compareToIgnoreCase("OptimisticUnchokingInterval") == 0)
+				{
+					if(!scanner.hasNextInt())
+						throw new Exception("Error:  Integer value required for the optimistically unchoked neighbor selection interval.");
+					else
+						timeOptUnchoke = scanner.nextInt();
+				}
+				else if(token.compareToIgnoreCase("FileName") == 0)
+				{
+					if(!scanner.hasNext())
+						throw new Exception("Error:  File name not correctly specified.");
+					else
+						fileName = scanner.next(); 
+				}
+				else if(token.compareToIgnoreCase("FileSize") == 0)
+				{
+					if(!scanner.hasNextInt())
+						throw new Exception("Error:  Integer value required for the file size.");
+					else
+						sizeOfFile = scanner.nextInt();
+				}	
+				else if(token.compareToIgnoreCase("PieceSize") == 0)
+				{
+					if(!scanner.hasNextInt())
+						throw new Exception("Error:  Integer value required for the piece size.");
+					else
+						sizeOfPiece = scanner.nextInt();
+				}
+				else
+				{
+					throw new Exception("Error.  Invalid string found in the common configuration file." + token);
+				}
+			}		
 		
-			// parse tokens from the file until the end of the file is hit
-	        do {
-	            token = tokenizer.nextToken();
-	            switch (token) {
-	               case StreamTokenizer.TT_EOF:
-	                  eof = true;
-	                  break;
-	               case StreamTokenizer.TT_EOL:
-	                  break;
-	               case StreamTokenizer.TT_WORD:
-	            	   if(tokenizer.sval.compareToIgnoreCase("NumberOfPreferredNeighbors") == 0)
-	            	   {
-	            		   token = tokenizer.nextToken();
-	            		   if(token == StreamTokenizer.TT_NUMBER)
-	            			   prefNeighbors = (int) tokenizer.nval;
-	            	   }
-	            	   else if(tokenizer.sval.compareToIgnoreCase("UnchokingInterval") == 0)
-	            	   {
-	            		   token = tokenizer.nextToken();
-	            		   if(token == StreamTokenizer.TT_NUMBER)
-	            			   timeUnchoke = (int) tokenizer.nval;
-	            	   }
-	            	   else if(tokenizer.sval.compareToIgnoreCase("OptimisticUnchokingInterval") == 0)
-	            	   {
-	            		   token = tokenizer.nextToken();
-	            		   if(token == StreamTokenizer.TT_NUMBER)
-	            			   timeOptUnchoke = (int) tokenizer.nval;
-	            	   }
-	            	   else if(tokenizer.sval.compareToIgnoreCase("FileName") == 0)
-	            	   {
-	            		   token = tokenizer.nextToken();
-	            		   if(token == StreamTokenizer.TT_WORD)
-	            			   fileName = tokenizer.sval;
-	            	   }
-	            	   else if(tokenizer.sval.compareToIgnoreCase("FileSize") == 0)
-	            	   {
-	            		   token = tokenizer.nextToken();
-	            		   if(token == StreamTokenizer.TT_NUMBER)
-	            			   sizeOfFile = (int) tokenizer.nval;
-	            	   }
-	            	   else if(tokenizer.sval.compareToIgnoreCase("PieceSize") == 0)
-	            	   {
-	            		   token = tokenizer.nextToken();
-	            		   if(token == StreamTokenizer.TT_NUMBER)
-	            			   sizeOfPiece = (int) tokenizer.nval;
-	            	   }
-	            	   else
-	            	   {
-	            		   throw new Exception("Error.  Invalid string found in the common configuration file.");
-	            	   }
-	            	   break;
-	               default:
-	               {
-	            	   throw new Exception("Error.  Invalid parameter name.");
-	               }
-	            }
-	         } while (!eof);
-
 			if(in != null)
 				in.close();
 			
@@ -166,8 +156,6 @@ public class PeerConfigs {
 				sizeOfLastPiece = sizeOfFile % sizeOfPiece;
 				totalPieces = sizeOfFile/sizeOfPiece + 1;
 			}
-			
-			//printCommonSettings();
 		}
 		catch(IOException exception)
 		{
@@ -178,108 +166,89 @@ public class PeerConfigs {
 	public void readPeerInfoConfig(String peerConfigPath) throws Exception
 	{
 		try{
-			int currByte = 0;
-			String currString = "";
-			int stringNum = 1;
-			totalPeers = 0;
-			
 			peerList = new ArrayList<Integer>();
 			hostList = new ArrayList<String>();
 			downloadPortList = new ArrayList<Integer>();
 			uploadPortList = new ArrayList<Integer>();
 			hasWholeFile = new ArrayList<Boolean>();
 			havePortList = new ArrayList<Integer>();
-			in = new FileInputStream(peerConfigPath);
-			streamReader = new BufferedReader(new InputStreamReader(in));
-			tokenizer = new StreamTokenizer(streamReader);
 			
-			int token;
+			
+			in = new FileInputStream(peerConfigPath);
+			Scanner scanner = new Scanner(in);
+
 			int tokenNum = 0;
 			int lineNum = 0;
 			int modResult;
 			
-			boolean eof = false;
-			
-			// parse tokens from the file until the end of the file is hit
-	        do {
-	            token = tokenizer.nextToken();
-	            ++tokenNum;
+			while(scanner.hasNext())
+			{
+				++tokenNum;
 	            lineNum = tokenNum/4;
 	            
 	            modResult = tokenNum % 4;
 	            
-	            switch (token) {
-	               case StreamTokenizer.TT_EOF:
-	                  eof = true;
-	                  break;
-	               case StreamTokenizer.TT_EOL:
-	            	   --tokenNum;
-	                  break;
-	               case StreamTokenizer.TT_WORD:
-	            	   if(modResult == 2) // token is the peer's host name
-	            	   {
-	            		   lineNum = (tokenNum-1)/4;
-	            		   hostList.add(lineNum, tokenizer.sval);	
-	            	   }
-	            	   else
-	            	   {
-	            		   throw new Exception("Error.  The host name in the wrong location.");
-	            	   }   
-	                  break;
-	               case StreamTokenizer.TT_NUMBER:
-	            	   if(modResult == 1) // token is the peer's peerID
-	            	   {
-	            		   lineNum = (tokenNum-1)/4;	            		   
-	            		   peerList.add(lineNum, (int) tokenizer.nval);	
-	            	   }
-	            	   else if(modResult == 3) // token is the peer's port #
-	            	   {
-	            		   if(tokenizer.nval < 0 || tokenizer.nval > 65535)
-	            		   {
-	            			   throw new Exception("Error.  Invalid port specified in the PeerInfo configuration file.");   
-	            		   }
-	            		   
-	            		   downloadPortList.add(lineNum, (int) tokenizer.nval);
-	            		   uploadPortList.add(lineNum, (int) tokenizer.nval + 1);
-	            		   havePortList.add(lineNum, (int) tokenizer.nval + 2);
-	            	   }
-	            	   else if(modResult == 0) // token indicates whether the peer has the whole file
-	            	   {
-	            		   ++totalPeers;
-	            		   
-	            		   if(tokenizer.nval == 0)
-	            			   hasWholeFile.add(false);
-	            		   else if(tokenizer.nval == 1)
-	            			   hasWholeFile.add(true);
-	            		   else
-	            		   {
-	            			   throw new Exception("Error.  You must specify either a 0 or 1 to denote if the peer has the entire file.");
-	            		   }
-	            	   }
-	            	   else
-	            	   {
-	            		   throw new Exception("Error.  Number argument in wrong location in the PeerInfo configuration file.");
-	            	   }
-	                  break;
-	               default:
-	            	   break;
-	            }
-	         } while (!eof);
+         	   if(modResult == 0) // token indicates whether the peer has the whole file
+         	   {
+         		   int wholeFile;
+         		   ++totalPeers;
+         		   
+         		   if(scanner.hasNextInt())
+         		   {
+         			   wholeFile = scanner.nextInt();
+         			   if(wholeFile == 1)
+         				   hasWholeFile.add(true);
+         			   else if(wholeFile == 0)
+         				   hasWholeFile.add(false);
+         		   }
+         		   else
+         			   throw new Exception("Error.  You must specify either a 0 or 1 to denote if the peer has the entire file.");
+         	   }
+         	   else if(modResult == 1) // token is the peer ID found in the current line of the file
+         	   {   
+        		   if(scanner.hasNextInt())
+         		   {
+            		   lineNum = (tokenNum-1)/4;	            		   
+            		   peerList.add(lineNum, scanner.nextInt());	
+         		   }  
+        		   else
+        			   throw new Exception("Error.  You must specify the peer id as an integer.");
+         	   }
+         	   else if(modResult == 2) // token is the host name/IP address found in the current line of the file
+         	   {
+        		   lineNum = (tokenNum-1)/4;
+        		   hostList.add(lineNum, scanner.next());
+         	   }	 
+        	   else if(modResult == 3) // token is the port number found in the current line of the file
+        	   {
+        		   int port;
+        		   
+        		   if(scanner.hasNextInt())
+        		   {
+        			   int portNum = scanner.nextInt();
+        			   
+            		   if(portNum < 0 || portNum > 65535)
+            		   {
+            			   throw new Exception("Error.  Invalid port specified in the PeerInfo configuration file.");   
+            		   }
+            		   
+            		   downloadPortList.add(lineNum, portNum);
+            		   uploadPortList.add(lineNum, portNum + 1);
+            		   havePortList.add(lineNum, portNum + 2);
+        		   }
+        		   else
+        			   throw new Exception("Error.  You must specify the port # as an integer.");  
+        	   }	           
+			}
 			
 			if(in != null)
 				in.close();
-			
-			if(totalPeers == 0)
-			{
-				throw new Exception("Error.  No peer information in the configuration file.");
-			}
-			//else
-				//printPeerInfo(totalPeers);
 		}
 		catch(IOException exception)
 		{
 			exception.printStackTrace();
 		}
+		
 	}
 	
 	// print info read from Common.cfg
